8a7e94b488f6983c57cf13763ab00cdd084aba1a
chenxi <chenxi@atguigu.com>
2023-03-14 20:47:57 +0800

    hot-fix commit 2

--- src/main/java/com/atguigu/main.java
+++ src/main/java/com/atguigu/main.java
@@ -6,5 +6,6 @@ public class main {
         System.out.println("你好");
         System.out.println("你不好");
         System.out.println("hello git");
+        System.out.println("hot-fix test");
     }
 }
